1aa2ecdadc78b68c5dc487b31309ec45d5362f8b
Susmita De <desusmita001@gmail.com>
2025-04-04 12:48:59 -0500

    Adding jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'jsdom', // Changed from 'node' to 'jsdom'
-  setupFilesAfterEnv: ['<rootDir>/frontend/src/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-};
